5e26e2a621d5b6d34cfd53fe55111846e126c904
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-05-29 14:56:15 -0400

    created user type class and its interface

---
+++ src/user/graphql/object-types/user.object-type.ts
@@ -0,0 +1,17 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { IUserType } from 'src/user/interfaces/object-types/user-object-type.interface';
+
+@ObjectType()
+export class UserType implements IUserType {
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  userName: string;
+
+  @Field()
+  profilePicture?: string;
+
+  @Field()
+  email: string;
+}
