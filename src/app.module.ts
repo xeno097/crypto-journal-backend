7d62e458bdf76c8700b951e4e4ba3a6d17930f3a
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-05-29 14:13:35 -0400

    added config module and mongoose module to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: './config/.env' }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    // GraphQLModule.forRoot({
+    //   autoSchemaFile: true,
+    //   context: ({ req, res }) => {
+    //     return req;
+    //   },
+    // }),
+  ],
   controllers: [],
   providers: [],
 })
