8ad5ff437fcb81061f3fc21cf91d67036e5be538
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-05-30 20:56:29 -0400

    modified user-result union type

--- src/user/graphql/union-types/user-result.union-type.ts
+++ src/user/graphql/union-types/user-result.union-type.ts
@@ -1,9 +1,18 @@
 import { createUnionType } from '@nestjs/graphql';
+import { InternalServerErrorType } from 'src/shared/graphql/errors/common/internal-server-error.object-type';
+import { UserNotFound } from '../../../shared/graphql/errors/user/user-not-found.object-type';
 import { UserType } from '../object-types/user.object-type';
 
 export const UserResultName = 'UserResult';
 
 export const UserResult = createUnionType({
   name: UserResultName,
-  types: () => [UserType],
+  types: () => [UserType, UserNotFound, InternalServerErrorType],
+  resolveType: value => {
+    if (value.errorType) {
+      return value.errorType;
+    }
+
+    return UserType;
+  },
 });
