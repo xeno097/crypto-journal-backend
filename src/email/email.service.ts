504e03d7a2a49accd0f2610a71f97f009c53890e
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-01 20:43:39 -0400

    created base email service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { Transporter } from 'nodemailer';
+import { EmailError } from 'src/errors/email.error';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private readonly _emailTransporter: Transporter;
+
+  constructor() {
+    this._emailTransporter = nodemailer.createTransport({
+      service: process.env.EMAIL_SERVICE,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_API_KEY,
+      },
+    });
+  }
+
+  public async sendEmail(sendEmailOptions: {
+    emailTo: string;
+    emailBody: string;
+    subject: string;
+  }) {
+    try {
+      const { emailTo, subject, emailBody } = sendEmailOptions;
+
+      await this._emailTransporter.sendMail({
+        from: process.env.EMAIL_FROM,
+        to: emailTo,
+        subject: subject,
+        html: emailBody,
+      });
+    } catch (error) {
+      console.log(error);
+      throw new EmailError();
+    }
+  }
+}
