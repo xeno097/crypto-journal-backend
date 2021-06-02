de374514721594067d49cc7f93ea12d6e9dd59a6
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-01 22:51:47 -0400

    exported user repository from user module and imported user module in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  providers: [AuthService, AuthResolver]
+  imports: [UserModule],
+  providers: [AuthService, AuthResolver],
 })
 export class AuthModule {}
