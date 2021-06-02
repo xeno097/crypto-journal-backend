663be60ccdc664fcbda5cf3658d2ab6f84c4554d
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-01 20:43:10 -0400

    created email error

---
+++ src/errors/email.error.ts
@@ -0,0 +1,9 @@
+import { BaseError } from './base-error.abstract-error';
+
+export class EmailError extends BaseError {
+  constructor() {
+    super();
+
+    Object.setPrototypeOf(this, EmailError);
+  }
+}
