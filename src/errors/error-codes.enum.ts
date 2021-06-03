5fadf8aaf90e70ba88d9b9b1b61391c267d6070f
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-03 19:01:29 -0400

    added jwt-expired error

--- src/errors/error-codes.enum.ts
+++ src/errors/error-codes.enum.ts
@@ -6,6 +6,7 @@ export enum ErrorCode {
   USER_NOT_FOUND = 'user-not-found',
   INVALID_SIGN_IN_TOKEN = 'invalid-signin-token',
   INVALID_JWT_FORMAT = 'invalid-jwt-format',
+  EXPIRED_JWT = 'expired-jwt',
 }
 
 registerEnumType(ErrorCode, {
