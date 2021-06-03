f1da6135d56eb266273172251b160cac7f8bfb9f
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-02 22:59:35 -0400

    implemented sign in flow

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UserModule } from 'src/user/user.module';
+import { FirebaseAdminService } from './firebase-admin.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService, AuthResolver],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.ACCESS_TOKEN_SECRET,
+      signOptions: {
+        expiresIn: process.env.ACCESS_TOKEN_EXP,
+      },
+    }),
+  ],
+  providers: [AuthService, AuthResolver, FirebaseAdminService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import { BaseError } from 'src/errors/base-error.abstract-error';
+import { ErrorCode } from 'src/errors/error-codes.enum';
+import { UserRepository } from 'src/user/user.repository';
+import { AuthPayloadDto } from './dtos/auth-payload.dto';
+import { SignInDto } from './dtos/sign-in.dto';
+import { FirebaseAdminService } from './firebase-admin.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayloadDto } from './dtos/jwt-payload.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userRepository: UserRepository,
+    private readonly firebaseAdminService: FirebaseAdminService,
+  ) {}
+
+  public async signIn(signInDto: SignInDto): Promise<[Error, AuthPayloadDto]> {
+    try {
+      // Get token
+      const { token } = signInDto;
+
+      // verify token
+      const createUserDto = await this.firebaseAdminService.verifyToken(token);
+      const { email } = createUserDto;
+
+      // throw an error if the token is invalid
+
+      // search the user by email in the db
+      let [err, user] = await this.userRepository.getOneEntity({ email });
+
+      if (err && (err as BaseError)?.code !== ErrorCode.USER_NOT_FOUND) {
+        return [err, null];
+      }
+
+      // if the user does not exist create it
+      if (!user) {
+        [err, user] = await this.userRepository.createEntity(createUserDto);
+      }
+
+      if (err) {
+        return [err, null];
+      }
+
+      // generate access token and refresh token
+
+      const { id, role } = user;
+      const jwtPayloadDto: JwtPayloadDto = { id, role, email };
+
+      const accessToken = this.jwtService.sign(jwtPayloadDto);
+      const refreshToken = this.jwtService.sign(jwtPayloadDto, {
+        secret: process.env.REFRESH_TOKEN_SECRET,
+        expiresIn: process.env.REFRESH_TOKEN_EXP,
+      });
+
+      // return access token, refresh token and user data
+      const authPayloadDto: AuthPayloadDto = {
+        accessToken,
+        refreshToken,
+        user,
+      };
+
+      return [null, authPayloadDto];
+    } catch (error) {
+      return [error, null];
+    }
+  }
+}
