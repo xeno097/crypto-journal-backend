c1d5ef8a9d361741309cdf9db7b1c36d4c2ba03a
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-03 18:38:42 -0400

    created invalid jwt format error

--- src/errors/error-codes.enum.ts
+++ src/errors/error-codes.enum.ts
@@ -5,6 +5,7 @@ export enum ErrorCode {
   INVALID_USER_INPUT = 'invalid-user-input',
   USER_NOT_FOUND = 'user-not-found',
   INVALID_SIGN_IN_TOKEN = 'invalid-signin-token',
+  INVALID_JWT_FORMAT = 'invalid-jwt-format',
 }
 
 registerEnumType(ErrorCode, {
---
+++ src/errors/invalid-jwt-format.error.ts
@@ -0,0 +1,13 @@
+import { BaseError } from './base-error.abstract-error';
+import { ErrorCode } from './error-codes.enum';
+
+export class InvalidJwtFormatError extends BaseError {
+  code = ErrorCode.INVALID_JWT_FORMAT;
+  message = 'Invalid jwt format';
+
+  constructor() {
+    super();
+
+    Object.setPrototypeOf(this, InvalidJwtFormatError.prototype);
+  }
+}
