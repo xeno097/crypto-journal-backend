882703ca9381afb6b1f6638b9869f56163e43275
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-04 22:57:08 -0400

    created common jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,26 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UserModule } from 'src/user/user.module';
 import { FirebaseAdminService } from './firebase-admin.service';
-import { JwtModule } from '@nestjs/jwt';
 import { GqlExceptionFilter } from 'src/shared/filters/graphql-exception.filter';
-import { ConfigService } from '@nestjs/config';
-import { EnvKey } from 'src/shared/enums/env-keys.enum';
+import { CommonJwtModule } from 'src/common-jwt/common-jwt.module';
 
 @Module({
-  imports: [
-    UserModule,
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          secret: configService.get(EnvKey.ACCESS_TOKEN_SECRET),
-          signOptions: {
-            expiresIn: configService.get(EnvKey.ACCESS_TOKEN_EXP),
-          },
-        };
-      },
-    }),
-  ],
+  imports: [UserModule, CommonJwtModule],
   providers: [
     AuthService,
     AuthResolver,
