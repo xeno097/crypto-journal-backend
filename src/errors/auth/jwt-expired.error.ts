bf53a9334a9acb8ef77168321a09433384970532
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-05 13:53:01 -0400

    moved invalid jwt expired error in a subfolder

--- src/errors/auth/jwt-expired.error.ts
+++ src/errors/auth/jwt-expired.error.ts
@@ -1,5 +1,5 @@
-import { BaseError } from './base-error.abstract-error';
-import { ErrorCode } from './error-codes.enum';
+import { BaseError } from '../base-error.abstract-error';
+import { ErrorCode } from '../error-codes.enum';
 
 export class JwtExpiredError extends BaseError {
   code = ErrorCode.EXPIRED_JWT;
