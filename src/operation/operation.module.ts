d1cbfccdd0ea404048dc1f8bb10fe5cdb464fb3d
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-10 22:44:20 -0400

    added operation entity to mongoose module

--- src/operation/operation.module.ts
+++ src/operation/operation.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { OperationService } from './operation.service';
 import { OperationResolver } from './operation.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  OperationEntity,
+  OperationEntitySchema,
+} from './database/operation.entity';
 
 @Module({
-  providers: [OperationService, OperationResolver]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: OperationEntity.name,
+        schema: OperationEntitySchema,
+      },
+    ]),
+  ],
+  providers: [OperationService, OperationResolver],
 })
 export class OperationModule {}
