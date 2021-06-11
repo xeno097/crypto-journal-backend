c2800d71a7a1846b94669e95f41363ce608822cb
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-10 23:15:01 -0400

    started working on operation repository

--- src/operation/operation.repository.ts
+++ src/operation/operation.repository.ts
@@ -1,4 +1,79 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { OperationEntity } from './database/operation.entity';
+import { OperationDto } from './dtos/operation.dto';
 
 @Injectable()
-export class OperationRepository {}
+export class OperationRepository {
+  constructor(
+    @InjectModel(OperationEntity.name)
+    private readonly operationModel: Model<OperationEntity>,
+  ) {}
+
+  private async _getOneEntity(getOneEntityDto: any): Promise<OperationEntity> {
+    try {
+      const entity = await this.operationModel.findOne(getOneEntityDto);
+
+      if (!entity) {
+        throw new Error();
+      }
+
+      return entity;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async getOneEntity(
+    getOneEntityDto: any,
+  ): Promise<[Error, OperationDto]> {
+    try {
+      const result = await this._getOneEntity(getOneEntityDto);
+
+      return [null, OperationEntity.toDto(result)];
+    } catch (error) {
+      return [error, null];
+    }
+  }
+
+  public async getEntities() {
+    try {
+    } catch (error) {}
+  }
+
+  public async countEntities(filter = {}) {
+    try {
+      const res = await this.operationModel.find(filter);
+
+      return [
+        null,
+        res.map(el => {
+          return OperationEntity.toDto(el);
+        }),
+      ];
+    } catch (error) {
+      return [error, null];
+    }
+  }
+
+  public async aggregateEntities() {
+    try {
+    } catch (error) {}
+  }
+
+  public async createEntity() {
+    try {
+    } catch (error) {}
+  }
+
+  public async updateEntity() {
+    try {
+    } catch (error) {}
+  }
+
+  public async deleteOneEntity() {
+    try {
+    } catch (error) {}
+  }
+}
