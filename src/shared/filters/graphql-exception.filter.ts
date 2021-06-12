b236e229994f49ac851b3b97cdc4be10604c4dc3
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-12 19:40:58 -0400

    added error management for http requests

--- src/shared/filters/graphql-exception.filter.ts
+++ src/shared/filters/graphql-exception.filter.ts
@@ -3,13 +3,32 @@ import { Catch, ExceptionFilter, Provider } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { GraphQLType } from 'graphql';
 import { getError } from '../graphql/utils/get-graphql-error.util';
+import { Response } from 'express';
+import { GqlContextType } from '@nestjs/graphql';
 
 @Catch()
 export class GraphqlExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
-    const { returnType } = host.getArgs().pop();
+    if (host.getType<GqlContextType>() === 'graphql') {
+      return this.graphqlHandler(exception, host);
+    }
 
-    if (isGraphQLListType((returnType as GraphQLType).toString())) {
+    return this.httpHandler(exception, host);
+  }
+
+  private httpHandler(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+
+    res.status(400).send(exception);
+  }
+
+  private graphqlHandler(exception: any, host: ArgumentsHost) {
+    const { returnType } = host.getArgs().pop();
+    if (
+      (returnType as GraphQLType)?.toString &&
+      isGraphQLListType((returnType as GraphQLType).toString())
+    ) {
       return [getError(exception)];
     }
 
