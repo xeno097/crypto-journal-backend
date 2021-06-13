b87ed554e8a04b4530561ffa2cbc41b563c8c50b
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-12 20:21:20 -0400

    added mongoose module to transaction module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionResolver } from './transaction.resolver';
 import { TransactionRepository } from './transaction.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  TransactionEntity,
+  TransactionEntitySchema,
+} from './database/transaction.entity';
+import { CommonJwtModule } from 'src/common-jwt/common-jwt.module';
 
 @Module({
+  imports: [
+    CommonJwtModule,
+    MongooseModule.forFeature([
+      {
+        name: TransactionEntity.name,
+        schema: TransactionEntitySchema,
+      },
+    ]),
+  ],
   providers: [TransactionResolver, TransactionService, TransactionRepository],
 })
 export class TransactionModule {}
