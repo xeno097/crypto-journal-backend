f0613a1da0e156dc98973a9a6a499ddfaf50d178
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-12 20:49:52 -0400

    added transactionTypeName constant

--- src/transaction/graphql/object-types/transaction.object-type.ts
+++ src/transaction/graphql/object-types/transaction.object-type.ts
@@ -2,7 +2,9 @@ import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { OperationType } from 'src/operation/graphql/object-types/operation.object-type';
 import { ITransactionType } from 'src/transaction/interfaces/object-types/transaction-object-type.interface';
 
-@ObjectType()
+export const transactionTypeName = 'Transaction';
+
+@ObjectType(transactionTypeName)
 export class TransactionType implements ITransactionType {
   @Field(() => ID)
   id: string;
