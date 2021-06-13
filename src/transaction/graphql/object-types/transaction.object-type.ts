d866fae90505c65a43982fb48c80fba2f5d4a7c4
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-12 20:36:27 -0400

    added transaction gql object type

---
+++ src/transaction/graphql/object-types/transaction.object-type.ts
@@ -0,0 +1,30 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { OperationType } from 'src/operation/graphql/object-types/operation.object-type';
+import { ITransactionType } from 'src/transaction/interfaces/object-types/transaction-object-type.interface';
+
+@ObjectType()
+export class TransactionType implements ITransactionType {
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  coinSymbol: string;
+
+  @Field()
+  coins: number;
+
+  @Field()
+  cost: number;
+
+  @Field()
+  fee: number;
+
+  @Field()
+  coinPrice: number;
+
+  @Field()
+  date: string;
+
+  @Field(() => OperationType)
+  operation: string;
+}
