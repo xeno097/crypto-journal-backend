04c4286520b2379d5644c2e3e6547e845ad46bb8
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-18 19:24:15 -0400

    added coin api response header enum

---
+++ src/coin-api/enum/coin-api-response-header-key.enum.ts
@@ -0,0 +1,5 @@
+export enum CoinApiResponseHedaderKey {
+  X_RATELIMIT_LIMIT = 'x-ratelimit-limit',
+  X_RATELIMIT_REMAINING = 'x-ratelimit-remaining',
+  X_RATELIMIT_RESET = 'x-ratelimit-reset',
+}
