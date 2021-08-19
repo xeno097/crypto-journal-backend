4c14a9b23b52b727c039046ef7e077ada02cd588
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-18 20:23:41 -0400

    implemented crypto currency service

--- src/crypto-currency/crypto-currency.service.ts
+++ src/crypto-currency/crypto-currency.service.ts
@@ -1,26 +1,118 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCryptoCurrencyInput } from './dtos/create-crypto-currency.dto';
-import { UpdateCryptoCurrencyInput } from './dtos/update-crypto-currency.dto';
+import { Injectable, Logger } from '@nestjs/common';
+import { CoinApiRepository } from 'src/coin-api/coin-api.repository';
+import { IAssetData } from 'src/coin-api/interfaces/asset-data.interface';
+import { IAssetIcon } from 'src/coin-api/interfaces/asset-icon.interface';
+import { BaseError } from 'src/errors/base-error.abstract-error';
+import { GetEntityByIdDto } from 'src/shared/dtos/get-entity-by-id.dto';
+import { CryptoCurrencyRepository } from './crypto-currency.repository';
+import { CryptoCurrencyDto } from './dtos/crypto-currency.dto';
+import { UpdateCryptoCurrencyDto } from './dtos/update-crypto-currency.dto';
+import { IFormattedAsset } from './interfaces/formatted-asset.interface';
 
 @Injectable()
 export class CryptoCurrencyService {
-  create(createCryptoCurrencyInput: CreateCryptoCurrencyInput) {
-    return 'This action adds a new cryptoCurrency';
+  constructor(
+    private readonly cryptoCurrencyRepository: CryptoCurrencyRepository,
+    private readonly coinApiRepository: CoinApiRepository,
+  ) {}
+
+  public async getOneCryptoCurrencyById(getOneEntityDto: GetEntityByIdDto) {
+    await this.updateCryptoCurrencyData();
+
+    return await this.cryptoCurrencyRepository.getOneEntity(getOneEntityDto);
   }
 
-  findAll() {
-    return `This action returns all cryptoCurrency`;
+  public async getAllCryptoCurrencies(): Promise<
+    [BaseError, CryptoCurrencyDto[]]
+  > {
+    await this.updateCryptoCurrencyData();
+
+    return await this.cryptoCurrencyRepository.getEntities();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} cryptoCurrency`;
+  public async updateCryptoCurrencyData(): Promise<[BaseError, boolean]> {
+    const logger = new Logger('CryptoCurrencyService');
+
+    logger.log('Updating CryptoCurrency data...');
+
+    const [err, asset] = await this.cryptoCurrencyRepository.getOneEntity({});
+
+    if (err) {
+      return [err, null];
+    }
+
+    const today = new Date(Date.now()).getDate();
+
+    if (asset.lastUpdated === today) {
+      logger.log('Already up to date');
+      return [null, true];
+    }
+
+    const [getCryptoErr, assets] = await this.getCryptoCurrencies();
+
+    if (getCryptoErr) {
+      return [getCryptoErr, null];
+    }
+
+    const updates: UpdateCryptoCurrencyDto[] = assets.map(asset => {
+      return {
+        getOneEntityDto: {
+          symbol: asset.symbol,
+        },
+        updateEntityPayload: {
+          icon: asset.icon,
+          price: asset.price,
+          name: asset.name,
+          symbol: asset.symbol,
+          lastUpdated: today,
+        },
+      };
+    });
+
+    const res = await this.cryptoCurrencyRepository.bulkUpdate(updates);
+
+    logger.log('CryptoCurrency data updated');
+    return [null, res];
   }
 
-  update(id: number, updateCryptoCurrencyInput: UpdateCryptoCurrencyInput) {
-    return `This action updates a #${id} cryptoCurrency`;
+  private async getCryptoCurrencies(): Promise<[BaseError, IFormattedAsset[]]> {
+    try {
+      const { data: icons } = await this.coinApiRepository.getAssetsIcons();
+      const { data: assets } = await this.coinApiRepository.getAssets();
+
+      const res = this.formatAssets(icons, assets);
+
+      return [null, res];
+    } catch (error) {
+      return [error, null];
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cryptoCurrency`;
+  private formatAssets(
+    iconsData: IAssetIcon[],
+    assetsData: IAssetData[],
+  ): IFormattedAsset[] {
+    const assetIconMap: Map<string, IAssetIcon> = new Map();
+
+    iconsData.forEach(el => {
+      assetIconMap.set(el.asset_id, el);
+    });
+
+    const formattedAssets: IFormattedAsset[] = [];
+
+    assetsData.forEach(el => {
+      if (el.type_is_crypto === 1) {
+        const asset: IFormattedAsset = {
+          name: el.name,
+          price: el.price_usd ?? 0,
+          symbol: el.asset_id,
+          icon: assetIconMap.get(el.asset_id)?.url,
+        };
+
+        formattedAssets.push(asset);
+      }
+    });
+
+    return formattedAssets;
   }
 }
