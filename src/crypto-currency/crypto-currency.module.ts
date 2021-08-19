1638926b698c9f2a9c35554fa561288d6516ed18
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-18 20:23:55 -0400

    implemented crypto currency resolver

--- src/crypto-currency/crypto-currency.module.ts
+++ src/crypto-currency/crypto-currency.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { CryptoCurrencyService } from './crypto-currency.service';
 import { CryptoCurrencyResolver } from './crypto-currency.resolver';
 import { CryptoCurrencyRepository } from './crypto-currency.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  CryptoCurrencyEntity,
+  CryptoCurrencyEntitySchema,
+} from './database/crypto-currency.entity';
+import { CoinApiModule } from 'src/coin-api/coin-api.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: CryptoCurrencyEntity.name,
+        schema: CryptoCurrencyEntitySchema,
+      },
+    ]),
+    CoinApiModule,
+  ],
   providers: [
     CryptoCurrencyResolver,
     CryptoCurrencyService,
--- src/crypto-currency/crypto-currency.resolver.ts
+++ src/crypto-currency/crypto-currency.resolver.ts
@@ -1,18 +1,43 @@
-import { Resolver, Query, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { FieldName } from 'src/shared/enums/input-fields.enum';
+import { idFieldOptions } from 'src/shared/graphql/options/id-input-field.options';
+import { getError } from 'src/shared/graphql/utils/get-graphql-error.util';
 import { CryptoCurrencyService } from './crypto-currency.service';
-import { CryptoCurrencyType } from './graphql/object-types/crypto-currency.object-type';
+import { CryptoCurrencyResult } from './graphql/union-types/crypto-currency-result.union-type';
 
-@Resolver(() => CryptoCurrencyType)
+@Resolver()
 export class CryptoCurrencyResolver {
   constructor(private readonly cryptoCurrencyService: CryptoCurrencyService) {}
 
-  @Query(() => [CryptoCurrencyType], { name: 'cryptoCurrency' })
-  findAll() {
-    return this.cryptoCurrencyService.findAll();
+  @Query(() => CryptoCurrencyResult)
+  public async getCryptoCurrencyById(
+    @Args(FieldName.ID, idFieldOptions) id: string,
+  ): Promise<typeof CryptoCurrencyResult> {
+    const [
+      err,
+      res,
+    ] = await this.cryptoCurrencyService.getOneCryptoCurrencyById({ id });
+
+    if (err) {
+      return getError(err);
+    }
+
+    return res;
   }
 
-  @Query(() => CryptoCurrencyType, { name: 'cryptoCurrency' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.cryptoCurrencyService.findOne(id);
+  @Query(() => [CryptoCurrencyResult])
+  public async getCryptoCurrencies(): Promise<
+    Array<typeof CryptoCurrencyResult>
+  > {
+    const [
+      err,
+      res,
+    ] = await this.cryptoCurrencyService.getAllCryptoCurrencies();
+
+    if (err) {
+      return [getError(err)];
+    }
+
+    return res;
   }
 }
