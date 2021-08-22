c3bf06f12c240c42b113eed76357897e35a11df1
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-22 13:00:41 -0400

    implemented get self holdings flow

--- src/holding/holding.resolver.ts
+++ src/holding/holding.resolver.ts
@@ -1,4 +1,28 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+import { JwtPayloadDto } from 'src/auth/dtos/jwt-payload.dto';
+import { GqlJwtPayload } from 'src/shared/decorators/jwt-payload.decorator';
+import { getError } from 'src/shared/graphql/utils/get-graphql-error.util';
+import { GqlAuthGuard } from 'src/shared/guards/gql-auth.guard';
+import { HoldingType } from './graphql/object-types/holding.object-type';
+import { HoldingResult } from './graphql/union-types/holding-result.union-type';
+import { HoldingService } from './holding.service';
 
-@Resolver()
-export class HoldingResolver {}
+@Resolver(() => HoldingType)
+@UseGuards(GqlAuthGuard)
+export class HoldingResolver {
+  constructor(private readonly holdingService: HoldingService) {}
+
+  @Query(() => [HoldingResult])
+  public async getSelfHoldings(
+    @GqlJwtPayload() jwtPayloadDto: JwtPayloadDto,
+  ): Promise<Array<typeof HoldingResult>> {
+    const [err, res] = await this.holdingService.getSelfHoldings(jwtPayloadDto);
+
+    if (err) {
+      return [getError(err)];
+    }
+
+    return res;
+  }
+}
--- src/holding/holding.service.ts
+++ src/holding/holding.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { JwtPayloadDto } from 'src/auth/dtos/jwt-payload.dto';
+import { BaseError } from 'src/errors/base-error.abstract-error';
+import { TransactionRepository } from 'src/transaction/transaction.repository';
+import { holdingAggregationPipeline } from './database/holding-aggregation-pipeline';
+import { HoldingDto } from './dtos/holding.dto';
 
 @Injectable()
-export class HoldingService {}
+export class HoldingService {
+  constructor(private readonly transactionRepository: TransactionRepository) {}
+
+  public async getSelfHoldings(
+    jwtPayload: JwtPayloadDto,
+  ): Promise<[BaseError, HoldingDto[]]> {
+    const { id } = jwtPayload;
+
+    const pipeline = holdingAggregationPipeline(id);
+
+    const res = await this.transactionRepository.aggregateEntities<HoldingDto>(
+      pipeline,
+    );
+
+    return res;
+  }
+}
